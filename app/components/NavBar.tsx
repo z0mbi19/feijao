c2c90e87d946f3e0fd680c7a32cfc0802de97b98
Mateus <z0mbi1998@outlook.com>
2023-07-09 23:41:27 -0300

    first page

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './nav.module.css'
+
+export default function NavBar() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.title} >
+        <h1>Pérola Corretora de cereais</h1>
+      </div>
+    </main>
+  )
+}
